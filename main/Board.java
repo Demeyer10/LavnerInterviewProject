05314a0c2fb7fd10544524f25b652062292ea4a5
Dustin Meyer <dustinmeyer@Dustins-MacBook-Air.local>
2023-02-22 13:46:20 -0500

    Drawing of tictactoe board

---
+++ main/Board.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Board {
+    public String[] boardValues = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+
+    public void Draw_Board() {
+        System.out.println();
+        System.out.printf("%s | %s | %s%n", this.boardValues[0], this.boardValues[1], this.boardValues[2]);
+        System.out.printf("--+---+--%n");
+        System.out.printf("%s | %s | %s%n", this.boardValues[3], this.boardValues[4], this.boardValues[5]);
+        System.out.printf("--+---+--%n");
+        System.out.printf("%s | %s | %s%n", this.boardValues[6], this.boardValues[7], this.boardValues[8]);
+        System.out.println();
+    }
+
+}
---
+++ main/TicTacToe.java
@@ -0,0 +1,8 @@
+package main;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.Draw_Board();
+    }
+}
